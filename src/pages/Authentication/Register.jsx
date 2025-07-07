ffab885728c7536e59b3124d2479fb4bfbaeb007
mahmud-reza-rafsun <rafsun16.it@gmail.com>
2025-07-07 08:00:27 +0600

    login register page responsive issue solved

--- src/pages/Authentication/Register.jsx
+++ src/pages/Authentication/Register.jsx
@@ -75,7 +75,6 @@ const Registration = () => {
     }
     catch (error) {
       toast.error(error.message);
-      console.log(error.message);
     }
   }
 
@@ -94,10 +93,10 @@ const Registration = () => {
             Get Your Free Account Now.
           </p>
 
-          <div className="flex justify-center items-center gap-4">
+          <div className="flex flex-col lg:flex-row justify-center items-center gap-0 lg:gap-4">
             <button
               onClick={handleGoogleSignIn}
-              className='flex w-3/4 cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50 '
+              className='flex w-full lg:w-3/4 cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50 '
             >
               <div className='px-4 py-2'>
                 <svg className='w-6 h-6' viewBox='0 0 40 40'>
@@ -120,11 +119,11 @@ const Registration = () => {
                 </svg>
               </div>
 
-              <span className='w-5/6 px-4 py-3 font-semibold text-center'>
+              <span className='w-5/6 px-0 lg:px-4 py-3 font-semibold text-center'>
                 Sign in with Google
               </span>
             </button>
-            <button onClick={handleGithibSignIn} className='py-3 w-1/4 flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50'>
+            <button onClick={handleGithibSignIn} className='py-3 w-full lg:w-1/4 flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg hover:bg-gray-50'>
               <svg className="w-6 h-6" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 .297C5.373.297 0 5.67 0 12.297c0 5.292 3.438 9.787 8.205 11.387.6.113.82-.26.82-.577 0-.285-.01-1.04-.015-2.04-3.338.726-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.756-1.333-1.756-1.09-.745.083-.729.083-.729 1.205.085 1.84 1.237 1.84 1.237 1.07 1.834 2.81 1.304 3.495.997.108-.776.418-1.305.76-1.605-2.665-.305-5.466-1.332-5.466-5.931 0-1.31.47-2.381 1.235-3.221-.124-.303-.535-1.524.116-3.176 0 0 1.008-.323 3.3 1.23a11.49 11.49 0 0 1 3.003-.404 11.49 11.49 0 0 1 3.003.404c2.29-1.553 3.295-1.23 3.295-1.23.653 1.652.242 2.873.118 3.176.77.84 1.233 1.911 1.233 3.221 0 4.61-2.804 5.624-5.475 5.921.43.372.823 1.104.823 2.222 0 1.606-.015 2.902-.015 3.296 0 .32.216.694.825.576C20.565 22.08 24 17.586 24 12.297 24 5.67 18.627.297 12 .297z" />
               </svg>
